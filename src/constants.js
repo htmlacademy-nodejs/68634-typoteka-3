443fa7f6bd814dd8a5ffc9560a094224c1a1eda1
evgeniy <not-found@inbox.ru>
2021-03-14 20:16:45 +0300

    В приложение добавлена обработка команды '--server'

--- src/constants.js
+++ src/constants.js
@@ -7,4 +7,11 @@ module.exports = {
     success: 0,
     fail: 1,
   },
+  HttpCode: {
+    OK: 200,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    FORBIDDEN: 403,
+    UNAUTHORIZED: 401,
+  }
 };
