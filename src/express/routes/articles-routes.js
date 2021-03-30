b9e34c0c54a1e865908f8541475f86190fecd365
evgeniy <not-found@inbox.ru>
2021-03-30 18:51:02 +0300

    Добавлены шаблоны страниц, обновлены маршруты

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,13 +4,13 @@ const {Router} = require(`express`);
 
 const articlesRouter = new Router();
 
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/${req.params.id}`));
+articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 articlesRouter.get(`/edit/:id`,
-    (req, res) => res.send(`/articles/edit/${req.params.id}`)
+    (req, res) => res.render(`new-post`)
 );
 articlesRouter.get(`/category/:id`,
-    (req, res) => res.send(`/articles/category/${req.params.id}`)
+    (req, res) => res.render(`articles-by-category`)
 );
 
 module.exports = articlesRouter;
