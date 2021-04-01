1ad6cb6dd3bbc377742199c476049f3ef20b3df6
evgeniy <not-found@inbox.ru>
2021-04-01 18:52:37 +0300

    Выполнена замена 'http-сервера' на 'Express-сервер'

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,51 +1,15 @@
 "use strict";
 
-const chalk = require(`chalk`);
-const http = require(`http`);
 const fs = require(`fs`).promises;
 const path = require(`path`);
-const {HttpCode} = require(`../../constants`);
+const express = require(`express`);
+const {Router} = require(`express`);
 
 const DEFAULT_PORT = 3000;
 const FILE_NAME = path.join(__dirname, `../../../mocks.json`);
 
-const sendResponce = (res, statusCode, message) => {
-  const template = `
-  <!Doctype html>
-    <html lang="ru">
-    <head>
-      <title>Listing of post titles</title>
-    </head>
-    <body>${message}</body>
-  </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponce(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponce(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-    default:
-      sendResponce(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
+const app = express();
+app.use(express.json());
 
 module.exports = {
   name: `--server`,
@@ -54,14 +18,18 @@ module.exports = {
     port = Number.parseInt(port, 10) || DEFAULT_PORT;
     port = (port <= 0) ? DEFAULT_PORT : port;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(chalk.red(`Ошибка при создании сервера`, err.message));
-        }
+    const routes = new Router();
+    routes.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        res.send([]);
+      }
+    });
 
-        return console.log(chalk.green(`Ожидаю соединений на ${port}`));
-      });
+    app.use(`/`, routes);
+    app.listen(port);
   }
 };
